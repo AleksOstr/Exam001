04d25805c66a89cf416816df4f02294b67f64b0e
AlexOstryanko <ostryankoam@yandex.ru>
2022-09-13 19:14:30 +0400

    add corrections to programm

--- Exam_Task/Program.cs
+++ Exam_Task/Program.cs
@@ -5,5 +5,6 @@ string[] result = new string[array.Length];
 for (int i = 0; i < array.Length; i++)
 {
     if (array[i].Length <= 3) result[i] = array[i];
-    Console.Write($"{result[i]} ");
+    else result[i] = string.Empty;
+    if (result[i] != string.Empty) Console.Write($"{result[i]} ");
 }
