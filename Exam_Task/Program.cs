f0d114e94111f8c33e58ea554df712306c00b739
AlexOstryanko <ostryankoam@yandex.ru>
2022-09-13 18:58:00 +0400

    finished program

---
+++ Exam_Task/Program.cs
@@ -0,0 +1,9 @@
+﻿// Программа, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+Console.Clear();
+string[] array = { "Rus", "QA", "number", "163", "Hello world!", "qwe" };
+string[] result = new string[array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3) result[i] = array[i];
+    Console.Write($"{result[i]} ");
+}
